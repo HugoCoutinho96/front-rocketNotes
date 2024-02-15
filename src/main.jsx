603045724278aade10cf077ed6d47f047204dd1c
Hugo Coutinho <hugocoutinho96@outlook.com>
2024-02-15 11:09:19 -0300

    building noteitem page

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Profile } from './Pages/Profile'
 import { Home } from './Pages/Home/'
-import { SignUp } from "./Pages/SignUp"
+import { New } from "./Pages/New"
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from "./styles/global.js"
 import theme from "./styles/theme.js"
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle/>
-      <Profile/>
+      <New/>
     </ThemeProvider>
   </React.StrictMode>,
 )
